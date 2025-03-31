c20f6865d7944c3a213e77545c92d9a0f47b39d7
MarMVbit <mmvillacorta@mail.austral.edu.ar>
2025-03-31 12:28:49 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,19 @@
 def change():
     expense = 23.75
     money = 100
+    vuelto = str(money - expense)
+    position = vuelto.find('.')
+    pesos =  vuelto[:position]
+    centavos = vuelto[position + 1: ]
+    print('Ingresar gasto:')
+    print(expense)
+    print('Dinero recibido:')
+    print(money)
+    print()
+    print('Vuelto')
+    print()
+    print('Pesos')
+    print(pesos)
+    print('centavos')
+    print(centavos)
+change()

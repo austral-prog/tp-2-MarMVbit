a950d6d0ccc615600def3cb41cfe3d03f8d73d5f
MarMVbit <mmvillacorta@mail.austral.edu.ar>
2025-03-31 18:11:24 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -2,8 +2,8 @@ def earth():
     x = "Bangladesh"
     y = "Barbados"
     if x>y:
-        print('The result of ' + x + ' comes first in the dictionary is ' + 'false')
-        print('The result of ' + y + ' comes first in the dictionary is ' + 'true')
+        print('The result of ' + x + ' comes first in the dictionary is ' + 'False.')
+        print('The result of ' + y + ' comes first in the dictionary is ' + 'True.')
     if x<y:
-        print('The result of ' + x + ' comes first in the dictionary is ' + 'true')
-        print('The result of ' + y + ' comes first in the dictionary is ' + 'false')
+        print('The result of ' + x + ' comes first in the dictionary is ' + 'True.')
+        print('The result of ' + y + ' comes first in the dictionary is ' + 'False.')
